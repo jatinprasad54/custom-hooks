014f6dec5b63da17027aadd05f6b7ce8b14ee40f
jatinprasad54 <jatinprasad54@gmail.com>
2024-10-02 19:27:32 +0530

    connection check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useOnlineStatus } from "./useOnlineStatus";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StatusBar />
+      <StatusButton />
     </div>
   );
 }
 
 export default App;
+
+function StatusBar() {
+  const isOnline = useOnlineStatus();
+  return <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>;
+}
+
+function StatusButton() {
+  const isOnline = useOnlineStatus();
+  return (
+    <button disabled={!isOnline}>{isOnline ? "Save" : "Reconnecting"}</button>
+  );
+}
