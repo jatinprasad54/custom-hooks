714e90ae099a890b71ed3855699dc459164b9db2
jatinprasad54 <jatinprasad54@gmail.com>
2024-10-02 19:50:18 +0530

    UseEffectEvent implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { useOnlineStatus } from "./useOnlineStatus";
+
+import ChatRoom from "./ChatRoom.js";
 function App() {
+  const [roomId, setRoomId] = useState("general");
   return (
-    <div className="App">
-      <StatusBar />
-      <StatusButton />
-    </div>
+    <>
+      <label>
+        Choose the chat room:{" "}
+        <select value={roomId} onChange={(e) => setRoomId(e.target.value)}>
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+      </label>
+      <hr />
+      <ChatRoom roomId={roomId} />
+    </>
   );
 }
 
 export default App;
 
-function StatusBar() {
-  const isOnline = useOnlineStatus();
-  return <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>;
-}
+// function StatusBar() {
+//   const isOnline = useOnlineStatus();
+//   return <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>;
+// }
 
-function StatusButton() {
-  const isOnline = useOnlineStatus();
-  return (
-    <button disabled={!isOnline}>{isOnline ? "Save" : "Reconnecting"}</button>
-  );
-}
+// function StatusButton() {
+//   const isOnline = useOnlineStatus();
+//   return (
+//     <button disabled={!isOnline}>{isOnline ? "Save" : "Reconnecting"}</button>
+//   );
+// }
